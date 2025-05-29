5e30b28eb61e6973a5a757fa81c2084b7369c24c
MadlyMoe <madlymoe@gmail.com>
2025-05-29 18:32:26 -0400

    Console app got create yippee

---
+++ src/FetchApi.py
@@ -0,0 +1,12 @@
+# FetchApi.py
+import requests
+import json
+
+def fetch_api(api_url):
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
