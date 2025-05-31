2362edf56bd22773a42b25ecfd074af46de3d166
Conan <conanjay@icloud.com>
2025-05-31 10:27:10 -0300

    Added icon to api, plus it now returns all herps

--- lib/hero-list.ts
+++ lib/hero-list.ts
@@ -1,33 +1,36 @@
 import axios from 'axios';
 
-// We could probably cache this for like an hour to give their api a break
-export async function heroPosition() {
+export async function numberOfHeros() {
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: 'https://mlbb-stats.ridwaanhall.com/api/hero-position/'
+        url: 'https://mlbb-stats.ridwaanhall.com/api/hero-list/'
+    };
+
+    const response = await axios.request(config);
+    
+    return Object.keys(response.data).length;
+}
+export async function heroPosition(heros: number) {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://mlbb-stats.ridwaanhall.com/api/hero-position/',
+        params: { size: heros }
     };
 
     const response = await axios.request(config);
     // get id name roles lanes smallmap
 
-
-    // data > records is where it splits between heros
-    console.log("I'm cooked");
-
     const list = response.data.data.records.map(hero => {
         let hero_id = hero.data.hero_id;
         let name = hero.data.hero.data.name;
-                // Make sure roadsort is an array and filter invalid items
         const lanes = (hero.data.hero.data.roadsort || [])
             .filter(item => item && item.data && item.data.road_sort_title)
             .map(item => item.data.road_sort_title);
-
-        // Same for sortid (roles)
         const roles = (hero.data.hero.data.sortid || [])
             .filter(item => item && item.data && item.data.sort_title)
             .map(item => item.data.sort_title);
-
         let banner = hero.data.hero.data.smallmap;
 
 
@@ -37,35 +40,44 @@ export async function heroPosition() {
     return list;
 }
 
-export async function heroRank() {
+export async function heroRank(heros: number) {
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: 'https://mlbb-stats.ridwaanhall.com/api/hero-rank/'
+        url: 'https://mlbb-stats.ridwaanhall.com/api/hero-rank/',
+        params: { size: heros }
     };
 
     const response = await axios.request(config);
     // get only names and icons
 
+    const list = response.data.data.records.map(hero => {
+        let name = hero.data.main_hero.data.name;
+        let icon = hero.data.main_hero.data.head;
 
-    // data > records[] > data > main_hero > data > name
-    // data > records[] > data > main_hero > data > head
+        return { name, icon };
+    })
 
-    return response.data;
+    return list;
 }
 
 
 export async function getList() {
 
-    // Game plan
-    // call hero rank for icon
-    const rank = await heroPosition();
+    let heros = await numberOfHeros();
+    // Rank for Icon
+    const rank = await heroRank(heros);
+    // Position for Name, ID, Roles, Lanes, Banner
+    const pos = await heroPosition(heros);
 
-    // call hero position for id, name, roles, lanes, banner
-    //const pos = await heroPosition();
+    const list = pos.map(hero => {
+        const rankHero = rank.find( item => item.name === hero.name);
 
-    // Get hero list
+        return {
+            ...hero,
+            icon: rankHero.icon ?? ""
+        };
+    });
 
-
-    return rank;
+    return list;
 }
