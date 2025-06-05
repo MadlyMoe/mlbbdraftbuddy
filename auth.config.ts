7efe324f5caefa7f3de0a6305f3b7c2f60c4bc31
Conan <conanjay@icloud.com>
2025-06-05 18:19:06 -0300

    Add role to User model

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,19 @@ import type { NextAuthConfig } from "next-auth";
 import Discord from "next-auth/providers/discord";
 import type { Provider } from "next-auth/providers";
 
-const providers: Provider[] = [Discord];
+const providers: Provider[] = [
+  Discord({
+    profile(profile) {
+      return {
+        id: profile.id,
+        name: profile.username,
+        email: profile.email,
+        image: profile.avatar,
+        role: "user", // 👈 default role
+      };
+    },
+  }),
+];
 
 export const authConfig = {
   providers,
@@ -15,9 +27,22 @@ export const authConfig = {
     signOut: "/",
   },
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role ?? "user";
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, user, token }) {
+      if (session.user && token?.role) {
+        session.user.id = user?.id ?? token?.sub ?? null;
+        session.user.role = token.role;
+      }
+      return session;
+    },
     authorized({ auth }) {
       const isAuthenticated = !!auth?.user;
-
       return isAuthenticated;
     },
   },
